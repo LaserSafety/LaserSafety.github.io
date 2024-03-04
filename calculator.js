35991e10fdfba8c1941f1e0281676a31203811a0
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-04 09:04:40 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,7 +3,13 @@ function calculate() {
     var wellenlaenge = parseFloat(document.getElementById('wellenlaenge').value);
     var C_e = parseFloat(document.getElementById('C_e').value);
     
-    var result = expositionsdauer * wellenlaenge * C_e;
+    var result;
+
+    if (expositionsdauer >= 1E-13 && expositionsdauer <= 1E-11) {
+        result = (1.5E-4) * C_e;
+    } else {
+        result = null;
+    }
     
     document.getElementById('result').innerHTML = "Result: " + result;
 }
