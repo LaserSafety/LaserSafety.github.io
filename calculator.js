75c7e32c957a5ecfa4382bee1fa8f1b0c348f7dc
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-04 10:02:50 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,36 +2,42 @@ function calculate() {
     var expositionsdauer_x = parseFloat(document.getElementById('expositionsdauer_x').value);
     var expositionsdauer_y = parseFloat(document.getElementById('expositionsdauer_y').value);
     var wellenlaenge = parseFloat(document.getElementById('wellenlaenge').value);
-    var C_e = parseFloat(document.getElementById('C_e').value);
+    var alpha = parseFloat(document.getElementById('alpha').value);
+    var alpha_max = parseFloat(document.getElementById('alphamax').value);
+    var alpha_min = parseFloat(document.getElementById('alphamin').value);
     
     var expositionsdauer = expositionsdauer_x * Math.pow(10, -expositionsdauer_y);
     
-    var result1, result2;
-    var formula1, formula2;
+    var result;
+    var formula;
     
     if (expositionsdauer === 1E-11) {
-        result1 = (1.5E-4) * C_e;
-        formula1 = "H = (1.5E-4) * C_e";
-        
-        result2 = (2.7E-4) * expositionsdauer * 0.75;
-        formula2 = "H = (2.7E-4) * t * 0.75, where t = expositionsdauer";
-        
-        var minResult = Math.min(result1, result2);
-        var minFormula = minResult === result1 ? formula1 : formula2;
-        
-        document.getElementById('result').innerHTML = "Result: " + minResult.toFixed(6) + " J*m^2" + "<br>Formula: " + minFormula;
+        result = (1.5E-4) * alpha;
+        formula = "H = (1.5E-4) * α";
     } else if (expositionsdauer >= 1E-13 && expositionsdauer < 1E-11) {
-        result1 = (1.5E-4) * C_e;
-        formula1 = "H = (1.5E-4) * C_e";
-        
-        document.getElementById('result').innerHTML = "Result: " + result1.toFixed(6) + " J*m^2" + "<br>Formula: " + formula1;
+        result = (1.5E-4) * alpha;
+        formula = "H = (1.5E-4) * α";
     } else if (expositionsdauer >= 1E-11 && expositionsdauer < 1E-9) {
-        result2 = (2.7E-4) * expositionsdauer * 0.75;
-        formula2 = "H = (2.7E-4) * t * 0.75, where t = expositionsdauer";
-        
-        document.getElementById('result').innerHTML = "Result: " + result2.toFixed(6) + " J*m^2" + "<br>Formula: " + formula2;
+        result = (2.7E-4) * expositionsdauer * 0.75;
+        formula = "H = (2.7E-4) * t * 0.75, where t = expositionsdauer";
     } else {
         document.getElementById('result').innerHTML = "Invalid expositionsdauer value";
+        return;
     }
-}
 
+    var C_e;
+    if (alpha >= 0 && alpha <= 1.5) {
+        C_e = 1;
+    } else if (alpha > 1.5 && alpha <= 100) {
+        C_e = alpha / 1.5;
+    } else if (alpha > 100 && alpha <= 1000) {
+        C_e = alpha_max / alpha_min;
+    } else {
+        document.getElementById('result').innerHTML = "Invalid α value";
+        return;
+    }
+
+    result *= C_e;
+
+    document.getElementById('result').innerHTML = "Result: " + result.toFixed(6) + " J*m^2" + "<br>Formula: " + formula + ", C_e = " + C_e.toFixed(6);
+}
