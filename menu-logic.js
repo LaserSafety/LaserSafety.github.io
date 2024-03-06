572b772cfa23242eebff584d9399d5e372fb0fed
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-06 13:06:01 +0100

    Update menu-logic.js

--- menu-logic.js
+++ menu-logic.js
@@ -3,20 +3,11 @@ function toggleMenu() {
   var settingsImg = document.querySelector(".rotate-settings");
   
   // Toggle menu visibility
-  function toggleMenu() {
-  var menu = document.getElementById("menu");
-
-  // Set menu display to block
-  menu.style.display = "block";
-}
-
-
-  // Toggle menu visibility
-  // if (menu.classList.contains("show")) {
-  // menu.classList.remove("show");
-  //  settingsImg.classList.remove("rotate");
-  // } else {
-  // menu.classList.add("show");
-  //  settingsImg.classList.add("rotate");
-  // }
+   if (menu.classList.contains("show")) {
+   menu.classList.remove("show");
+    settingsImg.classList.remove("rotate");
+   } else {
+   menu.classList.add("show");
+    settingsImg.classList.add("rotate");
+   }
 }
