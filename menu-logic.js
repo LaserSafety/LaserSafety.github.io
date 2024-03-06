2f172bda7eabc800e489fb95d773561031486b28
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-06 17:19:05 +0100

    Update menu-logic.js

--- menu-logic.js
+++ menu-logic.js
@@ -1,9 +1,6 @@
 function toggleMenu() {
-  var menu = document.getElementById("menu").style.display = "block";
+  var menu = document.getElementById("menu");
   var settingsImg = document.querySelector(".rotate-settings");
-
-  // Toggle menu visibility by toggling the 'show' class
   menu.classList.toggle("show");
-  
-  // Toggle rotation of the settings button
   settingsImg.classList.toggle("rotate");
+}
