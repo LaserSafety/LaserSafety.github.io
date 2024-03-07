ebeff7cb6d6353a389b7d24d58f0da19b1ba4466
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-07 12:23:18 +0100

    Update menu-logic.js

--- menu-logic.js
+++ menu-logic.js
@@ -3,14 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var menu = document.getElementById("menu");
   var settingsBtn = document.getElementById("settingsBtn");
   var settingsImg = settingsBtn.querySelector("img");
-
+  
   settingsBtn.addEventListener("click", function() {
-    if (menu.style.right === "0px") {
-      menu.style.right = "-300px";
-      settingsImg.classList.remove("rotate");
-    } else {
-      menu.style.right = "0px";
-      settingsImg.classList.add("rotate");
-    }
+    menu.classList.toggle("show"); // Toggle the 'show' class on the menu
+    settingsImg.classList.toggle("rotate"); // Toggle the 'rotate' class on the button
   });
 });
