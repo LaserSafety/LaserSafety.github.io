3d8380b3c96f5af38644c648e7e2edb3d978598e
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-14 12:47:10 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -318,7 +318,7 @@ if (coefficient >= 0.1 && coefficient < 1) {
     exponent--;
 }
 
-result = coefficient.toFixed(6) + " * 10^" + exponent; // Format in x * 10^y notation
+result = coefficient.toFixed(6) + " * 10<sup>" + exponent + "</sup>"; // Format in x * 10^y notation
 
 // Display result and formula
 document.getElementById('result').innerHTML = "Formula: " + formula + "<br>Result: " + result + " J/m<sup>2</sup>" + "<br>Optischer Bereich: " + optischer_bereich;
