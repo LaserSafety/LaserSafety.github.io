b2305ab02ce28bc645b6fa60a0251f764b2f7555
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-15 09:31:53 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+function derp() {
+        // Your calculation logic goes here
+        // For demonstration, let's say result is 42
+        var result = parseFloat(document.getElementById('wellenlaenge').value);
+        
+        document.getElementById('result').textContent = "Result: " + result;
+    }
