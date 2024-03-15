4e68924cbe7738cbc077cad43e7ed8aa5d6ff3ee
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-15 10:15:14 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-function derp() {
-        // Your calculation logic goes here
-        // For demonstration, let's say result is 42
-        var result = parseFloat(document.getElementById('wellenlaenge').value);
-        
-        document.getElementById('result').textContent = "Result: " + result;
-    }
+// calculator.js
+
+function calculate() {
+    // Your calculation logic goes here
+    // For demonstration, let's say result is 42
+    var result = 42;
+    document.getElementById('result').textContent = "Result: " + result;
+}
