14a4b6b747d5daebc1dff09fc5cb58955a113914
LaserSafety <161959238+LaserSafety@users.noreply.github.com>
2024-03-16 15:34:59 +0100

    Update main-logic.js

--- main-logic.js
+++ main-logic.js
@@ -1,11 +1,16 @@
 function openSettings() {
   document.getElementById("settingsMenu").style.display = "block";
 }
-
+ 
 function closeSettingsMenu() {
   document.getElementById("settingsMenu").style.display = "none";
 }
-
+function openModal() {
+        // Call calculate function and update result element
+    document.getElementById('id01').style.display='block';
+    var btn = document.getElementById("calculate");
+    btn.addEventListener("click", calculate);
+}
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -78,4 +83,26 @@ function move() {
     }
   }
 }
+    const Betriebsart = document.getElementById('betriebsart');
+    const Frequenz = document.getElementById('Frequenz');
+    const Pulsdauer = document.getElementById('Pulsdauer');
 
+    betriebsart.addEventListener('change', function() {
+      if (betriebsart.value === 'D') {
+        // Fade out first element, then fade out second element after a delay
+        Pulsdauer.classList.add('fade-out');
+        Pulsdauer.classList.remove('fade-in');
+        setTimeout(() => {
+          Frequenz.classList.add('fade-out');
+          Frequenz.classList.remove('fade-in');
+        }, 500); // Delay after first element fades out
+      } else {
+        // Fade in second element, then fade in first element after a delay
+          Frequenz.classList.remove('fade-out');
+          Frequenz.classList.add('fade-in');
+        setTimeout(() => {
+          Pulsdauer.classList.remove('fade-out');
+          Pulsdauer.classList.add('fade-in');
+        }, 500); // Delay before first element fades in
+      }
+    }
